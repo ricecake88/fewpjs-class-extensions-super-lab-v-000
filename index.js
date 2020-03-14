b68fbbb74079ef0b4db1729d3f841ded42344336
Grace Shih <vocalise@gmail.com>
2020-03-14 18:53:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,4 +25,8 @@ class Evergreen extends Tree {
     super(species);
     this.name = name;
   }
+  
+  static definition() {
+    return `${super.definition} Evergreens keep their leaves all year round.`;
+  }
 }
